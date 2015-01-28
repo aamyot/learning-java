72a8b3721fada120f061c805b3a5cd60a38a5066
Alexandre Amyot <aamyot@tritondigital.com>
2015-01-28 13:01:47 -0500

    learning xml cdata

---
+++ src/test/java/XmlTest.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.StringReader;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasXPath;
+import static org.junit.Assert.assertThat;
+
+public class XmlTest {
+
+    @Test
+    public void illegalCharactersAreAllowedInsideCDATA() {
+        String illegalCharacters = "&><";
+        String xml = "<path><![CDATA[" + illegalCharacters + "]]></path>";
+
+        assertThat(document(xml), hasXPath("/path", equalTo("&><")));
+    }
+
+    private Document document(String xml) {
+        try {
+            DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            return db.parse(new InputSource(new StringReader(xml)));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
