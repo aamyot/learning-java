27d816e6b997a837fef65c6522d6fab05dca3e68
Alexandre Amyot <aamyot@tritondigital.com>
2015-03-05 14:25:42 -0500

    A solution to the Diamond kata

--- src/test/java/com/alexandreamyot/kata/diamond/Diamond.java
+++ src/test/java/com/alexandreamyot/kata/diamond/Diamond.java
@@ -1,39 +1,57 @@
 package com.alexandreamyot.kata.diamond;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
+
+import static java.lang.String.valueOf;
+import static java.util.stream.Collectors.toList;
 
 public class Diamond {
 
-    private int size;
+    private final char endLetter;
 
-    public List<String> make(char endLetter) {
+    public Diamond(char endLetter) {
+        this.endLetter = endLetter;
+    }
+
+    public List<String> make() {
         List<String> diamond = new ArrayList<>();
 
-        size = height(endLetter);
-
-        if (endLetter == 'B') {
-            diamond.add(level('A'));
-            diamond.add("B.B");
-            diamond.add(level('A'));
-            return diamond;
+        for (char letter = 'A'; letter <= endLetter; letter++) {
+            diamond.add(line(letter));
         }
 
-        diamond.add(String.valueOf(endLetter));
-        return diamond;
+        return Stream.concat(diamond.stream(),
+                             diamond.stream()
+                                    .limit(diamond.size() - 1)
+                                    .sorted(Collections.reverseOrder())).collect(toList());
     }
 
-    private int height(char endLetter) {
-        return (endLetter - 'A') * 2 -1;
+    private String line(char letter) {
+        String level = level(letter);
+        String padding = spacing((lineSize() - level.length()) / 2);
+        return padding + level + padding;
     }
 
-    public String level(char letter) {
-        return paddig(size - letter) + letter + paddig(1);
+    protected String level(char letter) {
+        if (letter == 'A') return valueOf('A');
+
+        return valueOf(letter) + spacing(index(letter) * 2 - 1) + valueOf(letter);
     }
 
-    private String paddig(int paddingWidth) {
+    protected int lineSize() {
+        return (endLetter - 'A') * 2 + 1;
+    }
+
+    protected String spacing(int paddingWidth) {
         String pad = "";
         for (int i = 0; i < paddingWidth; i++) pad += ".";
         return pad;
+    }
+
+    private int index(char letter) {
+        return letter - 'A';
     }
 }
--- src/test/java/com/alexandreamyot/kata/diamond/DiamondTest.java
+++ src/test/java/com/alexandreamyot/kata/diamond/DiamondTest.java
@@ -3,20 +3,65 @@ package com.alexandreamyot.kata.diamond;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class DiamondTest {
 
     @Test
     public void diamondOfA() {
-        assertThat(new Diamond().make('A'), contains("A"));
+        assertThat(new Diamond('A').make(), contains("A"));
     }
 
     @Test
     public void diamondOfB() {
-        assertThat(new Diamond().make('B'), contains(
+        assertThat(new Diamond('B').make(), contains(
                 ".A.",
                 "B.B",
                 ".A."));
     }
+
+    @Test
+    public void diamondOfD() {
+        assertThat(new Diamond('D').make(), contains(
+                "...A...",
+                "..B.B..",
+                ".C...C.",
+                "D.....D",
+                ".C...C.",
+                "..B.B..",
+                "...A..."));
+    }
+
+    @Test
+    public void canPrintSpacing() {
+        assertThat(new Diamond('A').spacing(5), equalTo("....."));
+    }
+
+    @Test
+    public void lineSizeOfBShoulBe3() {
+        assertThat(new Diamond('B').lineSize(), equalTo(3));
+    }
+
+    @Test
+    public void lineSizeOfCShoulBe5() {
+        assertThat(new Diamond('C').lineSize(), equalTo(5));
+    }
+
+    @Test
+    public void levelOfA() {
+        assertThat(new Diamond('C').level('A'), equalTo("A"));
+    }
+
+    @Test
+    public void levelOfB() {
+        assertThat(new Diamond('D').level('B'), equalTo("B.B"));
+    }
+
+    @Test
+    public void levelOfD() {
+        assertThat(new Diamond('D').level('D'), equalTo("D.....D"));
+    }
+
+
 }
